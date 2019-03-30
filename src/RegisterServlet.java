d3bdcb5cfe09cd04c5c5c6c98bf1201ec38bb46f
Raghunadh <Raghunadh@Raghu>
2019-03-30 19:01:08 +0530

    Ready To Code 30 Mar 2019 7:00 PM

---
+++ src/RegisterServlet.java
@@ -0,0 +1,5 @@
+import javax.servlet.http.HttpServlet;
+
+public class RegisterServlet extends HttpServlet {
+
+}
---
+++ src/com/javatpoint/MyListener.java
@@ -0,0 +1,18 @@
+package com.javatpoint;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class MyListener implements ServletContextListener {
+
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void contextInitialized(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}//Listener class
