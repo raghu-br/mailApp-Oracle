1ce41802bfaeb3af959d9b261e5f12890e38149e
Raghunadh <Raghunadh@Raghu>
2019-03-30 13:01:39 +0530

    1st committ

---
+++ src/DBIntializer.java
@@ -0,0 +1,7 @@
+//Oracle DB Details:-
+public interface DBIntializer {
+	String driver = "oracle.jdbc.driver.OracleDriver";
+	String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	String username = "system";
+	String password = "manager";
+}// interface
